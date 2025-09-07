0f63051c33f1d5118d3052aebd4cbb9b1f08a830
TwoThousand07 <aytimov.erzhanae@gmail.com>
2025-09-07 14:49:23 +0500

    Add Person model, and connect media folder for imagefield

--- about_me/models.py
+++ about_me/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Person(models.Model):
+  first_name = models.CharField(max_length=50, verbose_name='Имя')
+  last_name = models.CharField(max_length=50, verbose_name='Фамилия')
+  age = models.SmallIntegerField(default=18, verbose_name='Возраст')
+  email = models.EmailField(verbose_name='Почта')
+  image = models.ImageField(upload_to='profile_pics/', verbose_name='Фото профиля')
+  
--- main/settings.py
+++ main/settings.py
@@ -131,3 +131,7 @@ STATICFILES_PATH = BASE_DIR / 'static'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# MEDIA
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
